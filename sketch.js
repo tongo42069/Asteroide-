b5c48f5e7f1d7c13475d062b1ad9e717b4f1b8af
DragonskullerMK <markuskaer@gmail.com>
2020-01-16 10:20:17 +0100

    it move fast

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let ship;
 
+
 function setup ()  {
     createCanvas (windowWidth, windowHeight);
     ship = new Ship();
@@ -8,6 +9,23 @@ function setup ()  {
 function draw() {
     background (0);
     ship.render();
+    ship.turn();
+    ship.update();
+}
+
+function keyReleased() {
+    ship.setRotation(0);
+    ship.boosting(false);
+}
+
+function keyPressed() {
+    if (keyCode == RIGHT_ARROW){
+        ship.setRotation(0.1);
+    }else if (keyCode == LEFT_ARROW) {
+        ship.setRotation(-0.1);
+    }else if(keyCode == UP_ARROW) {
+        ship.boosting(true);
+    }
 }
 
 
@@ -15,17 +33,41 @@ function Ship()  {
     this.pos = createVector(width/2, height/2);
     this.r = 20;
     this.heading = PI / 2;
+    this.rotation = 0;
+    this.vel = createVector(0, 0);
+    this.isBoosting = false;
+
+    this.boosting = function(b) {
+        this.isBoosting = b;
+    }
+
+    this.update = function() {
+        if (this.isBoosting) {
+            this.boost();
+        }
+        this.pos.add(this.vel);
+        this.vel.mult(0.95);
+    }
+
+    this.boost = function() {
+        let force = p5.Vector.fromAngle(this.heading);
+        this.vel.add(force);
+    }
 
     this.render = function()  {
         translate(this.pos.x, this.pos.y);
-        rotate(this.heading);
+        rotate(this.heading + PI / 2);
         noFill();
         stroke(255);
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
     }
 
-    this.turn = function(angle) {
-        this.heading += angle;
+    this.setRotation = function(a) {
+        this.rotation = a;
+    }
+
+    this.turn = function() {
+        this.heading += this.rotation;
     
     }
 

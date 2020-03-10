0461e808e39efbc3ad7ab11db215fa152e77cfb8
DragonskullerMK <markuskaer@gmail.com>
2020-03-10 23:40:13 +0100

    worst monday of my life

--- ship.js
+++ ship.js
@@ -1,90 +1,67 @@
-let ship;
-
-
-function setup ()  {
-    createCanvas (windowWidth, windowHeight);
-    ship = new Ship();
-}
-
-function draw() {
-    background (0);
-    ship.render();
-    ship.turn();
-    ship.update();
-    ship.edges();
-}
-
-function keyReleased() {
-    ship.setRotation(0);
-    ship.boosting(false);
-}
-
-function keyPressed() {
-    if (keyCode == RIGHT_ARROW){
-        ship.setRotation(0.1);
-    }else if (keyCode == LEFT_ARROW) {
-        ship.setRotation(-0.1);
-    }else if(keyCode == UP_ARROW) {
-        ship.boosting(true);
+function Ship() {
+  this.pos = createVector(width/2, height/2);
+  this.r = 20;
+  this.heading = 0;
+  this.rotation = 0;
+  this.vel = createVector(0, 0);
+  this.isBoosting = false;
+  
+  this.boosting = function(b) {
+    this.isBoosting = b;
+  }
+  
+  this.update = function() {
+    if (this.isBoosting) {
+    	this.boost();
     }
-}
-
-
-function Ship()  {
-    this.pos = createVector(width/2, height/2);
-    this.r = 20;
-    this.heading = PI / 2;
-    this.rotation = 0;
-    this.vel = createVector(0, 0);
-    this.isBoosting = false;
-
-    this.boosting = function(b) {
-        this.isBoosting = b;
+    this.pos.add(this.vel);
+    this.vel.mult(0.99);
+  }
+  
+  this.boost = function() {
+    var force = p5.Vector.fromAngle(this.heading);
+    force.mult(0.1);
+    this.vel.add(force);
+  }
+  
+  this.hits = function(asteroid) {
+    var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
+    if (d < this.r + asteroid.r) {
+      return true;
+    } else {
+      return false;
     }
-
-    this.update = function() {
-        if (this.isBoosting) {
-            this.boost();
-        }
-        this.pos.add(this.vel);
-        this.vel.mult(0.99);
+  }
+  
+  this.render = function() {
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.heading + PI / 2);
+    fill(0);
+    stroke(255);
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+    pop();
+  }
+  
+  this.edges = function() {
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    } else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
     }
-
-    this.boost = function() {
-        let force = p5.Vector.fromAngle(this.heading);
-        force.mult(0.1);
-        this.vel.add(force);
-    }
-
-    this.render = function()  {
-        translate(this.pos.x, this.pos.y);
-        rotate(this.heading + PI / 2);
-        noFill();
-        stroke(255);
-        triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
-    }
-
-    this.edges = function() {
-        if (this.pos.x > width + this.r) {
-        this.pos.x = -this.r;
-        } else if (this.pos.x < -this.r){
-         this.pos.x = width + this.r;
-        }
     if (this.pos.y > height + this.r) {
-         this.pos.y = -this.r;
-        } else if (this.pos.y < -this.r){
-         this.pos.y = height + this.r;
-        }
+      this.pos.y = -this.r;
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
     }
-
-    this.setRotation = function(a) {
-        this.rotation = a;
-    }
-
-    this.turn = function() {
-        this.heading += this.rotation;
-    
-    }
-
+  }
+  
+  this.setRotation = function(a) {
+    this.rotation = a;
+  }
+  
+  this.turn = function() {
+    this.heading += this.rotation;
+  }
+  
 }
-
--- sketch.js
+++ sketch.js
@@ -1,95 +1,70 @@
-let ship;
-let asteroids = [];
+var ship;
+var asteroids = [];
+var lasers = [];
 
-function setup ()  {
-    createCanvas (windowWidth, windowHeight);
-    ship = new Ship();
-    asteroids.push(new Asteroid());
-}
+function setup() { 
+  createCanvas(windowWidth, windowHeight);
+  ship = new Ship();
+  for (var i = 0; i < 5; i++) {
+  	asteroids.push(new Asteroid());
+  }
+} 
 
-function draw() {
-    background (0);
-    ship.render();
-    ship.turn();
-    ship.update();
-    ship.edges();
-
-    for(let i = 0; i < asteroids.length; i++) {
-        asteroids[i].render();
+function draw() { 
+  background(0);
+  
+  for (var i = 0; i < asteroids.length; i++) {
+    if (ship.hits(asteroids[i])) {
+      console.log('ooops!');
     }
+    asteroids[i].render();
+  	asteroids[i].update();
+    asteroids[i].edges();
+  }
+  
+  for (var i = lasers.length-1; i >= 0; i--) {
+    lasers[i].render();
+  	lasers[i].update();
+    if (lasers[i].offscreen()) {
+      lasers.splice(i, 1);
+    } else {
+    	for (var j = asteroids.length-1; j >= 0; j--) {
+    		if (lasers[i].hits(asteroids[j])) {
+    	    if (asteroids[j].r > 10) {
+    	  		var newAsteroids = asteroids[j].breakup();
+    	    	asteroids = asteroids.concat(newAsteroids);
+    	    }
+    	    asteroids.splice(j, 1);
+    	    lasers.splice(i, 1);
+    	    break;
+    		}
+    	}
+    }
+  }
+  
+  console.log(lasers.length);
+  
+  ship.render();
+  ship.turn();
+  ship.update();
+  ship.edges();
+  
+  
 }
 
 function keyReleased() {
-    ship.setRotation(0);
-    ship.boosting(false);
+  ship.setRotation(0);
+  ship.boosting(false);
 }
 
 function keyPressed() {
-    if (keyCode == RIGHT_ARROW){
-        ship.setRotation(0.1);
-    }else if (keyCode == LEFT_ARROW) {
-        ship.setRotation(-0.1);
-    }else if(keyCode == UP_ARROW) {
-        ship.boosting(true);
-    }
+  if (key == ' ') {
+  	lasers.push(new Laser(ship.pos, ship.heading));
+	} else if (keyCode == RIGHT_ARROW) {
+    ship.setRotation(0.1);
+  } else if (keyCode == LEFT_ARROW) {
+    ship.setRotation(-0.1);
+  } else if (keyCode == UP_ARROW) {
+    ship.boosting(true);
+  }
 }
-
-
-function Ship()  {
-    this.pos = createVector(width/2, height/2);
-    this.r = 20;
-    this.heading = PI / 2;
-    this.rotation = 0;
-    this.vel = createVector(0, 0);
-    this.isBoosting = false;
-
-    this.boosting = function(b) {
-        this.isBoosting = b;
-    }
-
-    this.update = function() {
-        if (this.isBoosting) {
-            this.boost();
-        }
-        this.pos.add(this.vel);
-        this.vel.mult(0.99);
-    }
-
-    this.boost = function() {
-        let force = p5.Vector.fromAngle(this.heading);
-        force.mult(0.1);
-        this.vel.add(force);
-    }
-
-    this.render = function()  {
-        translate(this.pos.x, this.pos.y);
-        rotate(this.heading + PI / 2);
-        noFill();
-        stroke(255);
-        triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
-    }
-
-    this.edges = function() {
-        if (this.pos.x > width + this.r) {
-        this.pos.x = -this.r;
-        } else if (this.pos.x < -this.r){
-         this.pos.x = width + this.r;
-        }
-    if (this.pos.y > height + this.r) {
-         this.pos.y = -this.r;
-        } else if (this.pos.y < -this.r){
-         this.pos.y = height + this.r;
-        }
-    }
-
-    this.setRotation = function(a) {
-        this.rotation = a;
-    }
-
-    this.turn = function() {
-        this.heading += this.rotation;
-    
-    }
-
-}
-
